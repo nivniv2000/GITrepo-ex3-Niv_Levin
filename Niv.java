e4929ce902ce7a67b1d3bc6a3f532184662df82c
nivniv2000 <nivlevin2000@gmail.com>
2026-04-27 18:26:54 +0300

    update Niv print

--- Niv.java
+++ Niv.java
@@ -1,7 +1,7 @@
 public class Niv {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Niv");
+            System.out.println("Niv Jenkins update " + i);
         }
     }
 }
